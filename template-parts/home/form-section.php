32e7d574fba43b45074cf0a624e9155a714cb579
Joan <st.joan3007@gmail.com>
2024-06-14 04:23:27 +0700

    Optimize button, footer

---
+++ template-parts/home/form-section.php
@@ -0,0 +1,50 @@
+<form id="form-section" class="form-container">
+    <div class="form-section-wrapper">
+        <div class="form-title">
+            <h3>Hey! Tell Us All The Things</h3>
+        </div>
+        <div class="form-info">
+            <div class="form-prefix">
+                <label for="fprefix" class="field-title">Mr/Mrs/Ms <span style="color: red">*</span></label>
+                <input type="text" id="fprefix" name="fprefix" class="field-input">
+            </div>
+            <div class="form-name">
+                <label for="fname" class="field-title">Your Name <span style="color: red">*</span></label>
+                <input type="text" id="fname" name="fname" class="field-name">
+            </div>
+            <div class="form-email">
+                <label for="femail" class="field-title">Your Email <span style="color: red">*</span></label>
+                <input type="email" id="femail" name="femail" class="field-email">
+            </div>
+            <div class="form-phone">
+                <label for="fphone" class="field-title">Your Phone Number</label>
+                <input type="tel" id="fphone" name="fphone" class="field-phone">
+            </div>
+            <div class="form-industry">
+                <label for="findustry" class="field-title">Your Company Industry <span
+                        style="color: red">*</span></label>
+                <input type="text" id="findustry" name="findustry" class="field-industry">
+            </div>
+            <div class="form-message">
+                <label for="fmessage" class="field-title">Share With Us Your Project <span
+                        style="color: red">*</span></label>
+                <input type="text" id="fmessage" name="fmessage" class="field-message">
+            </div>
+            <div class="form-interest">
+                <label for="finterest" class="field-title">Which SIDEKICK Services Are You Interested In?</label>
+                <select type="text" id="finterest" name="finterest" class="field-interest">
+                    <option value="media-strategy&management">media strategy & management</option>
+                    <option value="branding&communication">branding & ommunication</option>
+                    <option value="digital-transformation">digital transformation</option>
+                    <option value="website&app-development">website & app development</option>
+                    <option value="marketing-data-analytics">marketing data analytics</option>
+                    <option value="custom-solutions">custom solutions</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-footer">
+            <div class="form-sk-email"></div>
+            <button class="form-button"></button>
+        </div>
+    </div>
+</form>
